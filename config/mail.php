1d60b6a1f394e1d75e5b9bbb4e449f323b9a5031
GBursali <gurkanbursali@hotmail.com>
2020-04-26 01:03:17 +0000

    mail.live

--- config/mail.php
+++ config/mail.php
@@ -83,7 +83,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_USERNAME', 'hello@example.com'),
         'name' => env('MAIL_TITLE', 'Example'),
     ],
 
